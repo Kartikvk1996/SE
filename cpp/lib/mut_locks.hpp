788b7c3f58bddd88920b33a4058be93556fac851
Kartikvk1996 <karthikkalaghatgi123@gmail.com>
2018-04-11 22:26:15 +0530

    lib

--- cpp/lib/mut_locks.hpp
+++
@@ -1,8 +0,0 @@
-// mutex locks
-#include<mutex>
-
-mutex mutex_crawlerInfo;
-mutex mutex_poll;
-
-
-mutex mutex_queLock;
--- cpp/lib/proto/intropdu.cpp
+++
@@ -1,7 +0,0 @@
-#include "proto/intropdu.hpp"
-
-IntroPDU::IntroPDU(string guest_host, string guest_port) {
-    setMethod(METHOD_INTRO);
-    jdata[GUEST_HOST] = guest_host;
-    jdata[GUEST_PORT] = guest_port;
-}
